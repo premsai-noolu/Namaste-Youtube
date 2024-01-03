c0a6016f87bbb2da98fc602caaf121ebb49e1ec2
premsai-noolu <premsainoolu01@gmail.com>
2024-01-03 13:20:17 +0530

    api polling

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -6,26 +6,73 @@ const commentsData = [
     text: "He is a good boy",
     replies: [],
   },
+  {
+    name: "prem",
+    text: "He is a good boy",
+    replies: [
+      {
+        name: "prem",
+        text: "He is a good boy",
+        replies: [
+          {
+            name: "prem",
+            text: "He is a good boy",
+            replies: [],
+          },
+          {
+            name: "prem",
+            text: "He is a good boy",
+            replies: [],
+          },
+        ],
+      },
+      {
+        name: "prem",
+        text: "He is a good boy",
+        replies: [],
+      },
+    ],
+  },
+  {
+    name: "prem",
+    text: "He is a good boy",
+    replies: [],
+  },
 ];
 
 const Comment = ({ data }) => {
   const { name, text, replies } = data;
   return (
-    <div>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" />
-      <div>
-        <p>{name}</p>
+    <div className="flex shadow-sm bg-gray-100 p-2 rounded-lg my-2">
+      <img
+        className="w-12 h-12"
+        alt="user"
+        src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
+      />
+      <div className="px-3">
+        <p className="font-bold">{name}</p>
         <p>{text}</p>
       </div>
     </div>
   );
 };
 
+const CommentsList = ({ comments }) => {
+  return comments.map((comment, index) => (
+    <div key={index}>
+      <Comment data={comment} />
+      <div className="pl-5 border border-l-black ml-5">
+        <CommentsList comments={comment.replies} />
+      </div>
+    </div>
+  ));
+};
+
 const CommentsContainer = () => {
   return (
     <div className="m-5 p-2">
       <h1 className="text-2xl font-bold ">Comments</h1>
-      <Comment data={commentsData[0]} />
+      <CommentsList comments={commentsData} />
     </div>
   );
 };
